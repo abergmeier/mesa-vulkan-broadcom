51c02d5dd63fc62d4eb60c45ae6c3116de3d746c
Andreas Bergmeier <abergmeier@gmx.net>
2019-11-17 21:26:14 +0100

    Introduced closing of GEM
    Code taken from Gallium

--- src/broadcom/vulkan/v3dvk_gem.c
+++ src/broadcom/vulkan/v3dvk_gem.c
@@ -1,5 +1,23 @@
 
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <xf86drm.h>
+#include <drm/msm_drm.h>
+#include "device.h"
 #include "v3dvk_gem.h"
+
+void
+v3dvk_gem_close(const struct v3dvk_device *dev, uint32_t gem_handle)
+{
+   struct drm_gem_close req = {
+      .handle = gem_handle,
+   };
+
+   int ret = drmIoctl(dev->fd, DRM_IOCTL_GEM_CLOSE, &req);
+   if (ret != 0)
+           fprintf(stderr, "close object %d: %s\n", gem_handle, strerror(errno));
+}
 
 int
 v3dvk_gem_gpu_get_reset_stats(struct v3dvk_device *device,
--- src/broadcom/vulkan/v3dvk_gem.h
+++ src/broadcom/vulkan/v3dvk_gem.h
@@ -6,6 +6,9 @@
 
 struct v3dvk_device;
 
+void
+v3dvk_gem_close(const struct v3dvk_device *dev, uint32_t gem_handle);
+
 int v3dvk_gem_gpu_get_reset_stats(struct v3dvk_device *device,
                                   uint32_t *active, uint32_t *pending);
 
