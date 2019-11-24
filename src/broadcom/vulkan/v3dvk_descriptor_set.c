39f1a616ac5751e0a85c64b17d67046ac781f3cb
Andreas Bergmeier <abergmeier@gmx.net>
2019-11-24 20:45:42 +0100

    Initialize stubs.

--- src/broadcom/vulkan/v3dvk_descriptor_set.c
+++ src/broadcom/vulkan/v3dvk_descriptor_set.c
@@ -2,10 +2,123 @@
 #include <assert.h>
 #include "common.h"
 #include "vk_alloc.h"
+#include "vk_util.h"
 #include "device.h"
 #include "v3dvk_descriptor_set.h"
 #include "v3dvk_entrypoints.h"
 #include "v3dvk_error.h"
+
+VkResult
+v3dvk_CreateDescriptorSetLayout(
+   VkDevice _device,
+   const VkDescriptorSetLayoutCreateInfo *pCreateInfo,
+   const VkAllocationCallbacks *pAllocator,
+   VkDescriptorSetLayout *pSetLayout)
+{
+   V3DVK_FROM_HANDLE(v3dvk_device, device, _device);
+   struct v3dvk_descriptor_set_layout *set_layout;
+
+   assert(pCreateInfo->sType ==
+          VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO);
+   const VkDescriptorSetLayoutBindingFlagsCreateInfoEXT *variable_flags =
+      vk_find_struct_const(
+         pCreateInfo->pNext,
+         DESCRIPTOR_SET_LAYOUT_BINDING_FLAGS_CREATE_INFO_EXT);
+
+   uint32_t max_binding = 0;
+   for (uint32_t j = 0; j < pCreateInfo->bindingCount; j++)
+      max_binding = MAX2(max_binding, pCreateInfo->pBindings[j].binding);
+
+   uint32_t size =
+      sizeof(struct v3dvk_descriptor_set_layout) +
+      (max_binding + 1) * sizeof(set_layout->binding[0]);
+
+   set_layout = vk_alloc2(&device->alloc, pAllocator, size, 8,
+                          VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
+   if (!set_layout)
+      return v3dvk_error(device->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+
+   set_layout->flags = pCreateInfo->flags;
+#if 0
+   /* We just allocate all the samplers at the end of the struct */
+   uint32_t *samplers = (uint32_t *) &set_layout->binding[max_binding + 1];
+   (void) samplers; /* TODO: Use me */
+
+   VkDescriptorSetLayoutBinding *bindings = create_sorted_bindings(
+      pCreateInfo->pBindings, pCreateInfo->bindingCount);
+   if (!bindings) {
+      vk_free2(&device->alloc, pAllocator, set_layout);
+      return v3dvk_error(device->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+   }
+
+   set_layout->binding_count = max_binding + 1;
+   set_layout->shader_stages = 0;
+   set_layout->dynamic_shader_stages = 0;
+   set_layout->has_immutable_samplers = false;
+   set_layout->size = 0;
+
+   memset(set_layout->binding, 0,
+          size - sizeof(struct tu_descriptor_set_layout));
+
+   uint32_t buffer_count = 0;
+   uint32_t dynamic_offset_count = 0;
+
+   for (uint32_t j = 0; j < pCreateInfo->bindingCount; j++) {
+      const VkDescriptorSetLayoutBinding *binding = bindings + j;
+      uint32_t b = binding->binding;
+      uint32_t alignment = 8;
+      unsigned binding_buffer_count = 1;
+
+      set_layout->size = align(set_layout->size, alignment);
+      set_layout->binding[b].type = binding->descriptorType;
+      set_layout->binding[b].array_size = binding->descriptorCount;
+      set_layout->binding[b].offset = set_layout->size;
+      set_layout->binding[b].buffer_offset = buffer_count;
+      set_layout->binding[b].dynamic_offset_offset = dynamic_offset_count;
+      set_layout->binding[b].size = descriptor_size(binding->descriptorType);
+
+      if (variable_flags && binding->binding < variable_flags->bindingCount &&
+          (variable_flags->pBindingFlags[binding->binding] &
+           VK_DESCRIPTOR_BINDING_VARIABLE_DESCRIPTOR_COUNT_BIT_EXT)) {
+         assert(!binding->pImmutableSamplers); /* Terribly ill defined  how
+                                                  many samplers are valid */
+         assert(binding->binding == max_binding);
+
+         set_layout->has_variable_descriptors = true;
+      }
+
+      set_layout->size +=
+         binding->descriptorCount * set_layout->binding[b].size;
+      buffer_count += binding->descriptorCount * binding_buffer_count;
+      dynamic_offset_count += binding->descriptorCount *
+                              set_layout->binding[b].dynamic_offset_count;
+      set_layout->shader_stages |= binding->stageFlags;
+   }
+
+   free(bindings);
+
+   set_layout->buffer_count = buffer_count;
+   set_layout->dynamic_offset_count = dynamic_offset_count;
+
+#endif
+   *pSetLayout = v3dvk_descriptor_set_layout_to_handle(set_layout);
+
+   return VK_SUCCESS;
+}
+
+void
+v3dvk_DestroyDescriptorSetLayout(VkDevice _device,
+                                 VkDescriptorSetLayout _set_layout,
+                                 const VkAllocationCallbacks *pAllocator)
+{
+   V3DVK_FROM_HANDLE(v3dvk_device, device, _device);
+   V3DVK_FROM_HANDLE(v3dvk_descriptor_set_layout, set_layout, _set_layout);
+
+   if (!set_layout)
+      return;
+
+   vk_free2(&device->alloc, pAllocator, set_layout);
+}
 
 
 /*
--- src/broadcom/vulkan/v3dvk_descriptor_set.h
+++ src/broadcom/vulkan/v3dvk_descriptor_set.h
@@ -3,7 +3,59 @@
 #define V3DVK_DESCRIPTOR_SET_H
 
 #include <stdint.h>
+#include <vulkan/vulkan.h>
 #include "v3dvk_constants.h"
+
+struct v3dvk_descriptor_set_binding_layout
+{
+   VkDescriptorType type;
+
+   /* Number of array elements in this binding */
+   uint32_t array_size;
+#if 0
+   uint32_t offset;
+   uint32_t buffer_offset;
+   uint16_t dynamic_offset_offset;
+
+   uint16_t dynamic_offset_count;
+   /* redundant with the type, each for a single array element */
+   uint32_t size;
+
+   /* Offset in the tu_descriptor_set_layout of the immutable samplers, or 0
+    * if there are no immutable samplers. */
+   uint32_t immutable_samplers_offset;
+#endif
+};
+
+struct v3dvk_descriptor_set_layout
+{
+   /* The create flags for this descriptor set layout */
+   VkDescriptorSetLayoutCreateFlags flags;
+#if 0
+
+   /* Number of bindings in this descriptor set */
+   uint32_t binding_count;
+
+   /* Total size of the descriptor set with room for all array entries */
+   uint32_t size;
+
+   /* Shader stages affected by this descriptor set */
+   uint16_t shader_stages;
+   uint16_t dynamic_shader_stages;
+
+   /* Number of buffers in this descriptor set */
+   uint32_t buffer_count;
+
+   /* Number of dynamic offsets used by this descriptor set */
+   uint16_t dynamic_offset_count;
+
+   bool has_immutable_samplers;
+   bool has_variable_descriptors;
+
+#endif
+   /* Bindings in this descriptor set */
+   struct v3dvk_descriptor_set_binding_layout binding[0];
+};
 
 struct v3dvk_pipeline_layout
 {
